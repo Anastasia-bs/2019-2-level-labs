6d924efc00efdbb2cefb8a4ced048fe34ec34a9a
Anastasia-bs <55382359+Anastasia-bs@users.noreply.github.com>
2019-09-16 11:45:29 +0300

    changes

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,6 +9,7 @@ def calculate_frequences(text: str) -> dict:
     Calculates number of times each word appears in the text
     """
     pass
+print('alo')
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
