96cf4b7228d45f3fd7d493895802bdcd1291127f
Anastasia-bs <anniebondarenko@gmail.com>
2019-11-08 17:11:42 +0300

    edited for lint check

--- lab_2/main.py
+++ lab_2/main.py
@@ -10,14 +10,12 @@ def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
 
 
 def initialize_edit_matrix(edit_matrix: tuple, add_weight: int, remove_weight: int) -> list:
-    m = edit_matrix
-    if m and isinstance(add_weight, int) and isinstance(remove_weight, int):
-        if len(m[0]):
-            for i in range(1, len(m)):
-                m[i][0] = m[i - 1][0] + remove_weight
-            for j in range(1, len(m[0])):
-                m[0][j] = m[0][j - 1] + add_weight
-    return list(m)
+    if edit_matrix and isinstance(add_weight, int) and isinstance(remove_weight, int) and edit_matrix[0]:
+        for i in range(1, len(edit_matrix)):
+            edit_matrix[i][0] = edit_matrix[i - 1][0] + remove_weight
+        for j in range(1, len(edit_matrix[0])):
+            edit_matrix[0][j] = edit_matrix[0][j - 1] + add_weight
+    return list(edit_matrix)
 
 
 def minimum_value(numbers: tuple) -> int:
@@ -30,19 +28,18 @@ def fill_edit_matrix(edit_matrix: tuple,
                      substitute_weight: int,
                      original_word: str,
                      target_word: str) -> list:
-    m = edit_matrix
     if original_word and isinstance(add_weight, int) and isinstance(remove_weight, int) \
             and isinstance(substitute_weight, int):
-        for i in range(1, len(m)):
-            for j in range(1, len(m[0])):
-                add = m[i][j-1] + add_weight
-                remove = m[i-1][j] + remove_weight
+        for i in range(1, len(edit_matrix)):
+            for j in range(1, len(edit_matrix[0])):
+                add = edit_matrix[i][j-1] + add_weight
+                remove = edit_matrix[i-1][j] + remove_weight
                 if original_word[i-1] == target_word[j-1]:
-                    sub = m[i-1][j-1]
+                    sub = edit_matrix[i-1][j-1]
                 else:
-                    sub = m[i-1][j-1] + substitute_weight
-                m[i][j] = minimum_value((add, remove, sub))
-    return list(m)
+                    sub = edit_matrix[i-1][j-1] + substitute_weight
+                edit_matrix[i][j] = minimum_value((add, remove, sub))
+    return list(edit_matrix)
 
 
 def find_distance(original_word: str,
@@ -65,18 +62,18 @@ def find_distance(original_word: str,
 
 
 def save_to_csv(edit_matrix: tuple, path_to_file: str) -> None:
-    with open(path_to_file, 'w') as f:
+    with open(path_to_file, 'w') as file:
         for line in edit_matrix:
-            for i in range(len(line)):
-                line[i] = str(line[i])
-            f.write(','.join(line) + '\n')
+            for i, num in enumerate(line):
+                line[i] = str(num)
+            file.write(','.join(line) + '\n')
 
 
 def load_from_csv(path_to_file: str) -> list:
-    with open(path_to_file) as m:
-        m = m.readlines()
-        for i in range(len(m)):
-            m[i] = m[i].split(',')
-            for j in range(len(m[i])):
-                m[i][j] = int(m[i][j])
-    return m
+    with open(path_to_file) as matrix:
+        matrix = matrix.readlines()
+        for i, string in enumerate(matrix):
+            matrix[i] = string.split(',')
+            for j, num in enumerate(matrix[i]):
+                matrix[i][j] = int(num)
+    return matrix
