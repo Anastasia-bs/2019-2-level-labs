fe91272bab1713b1114c99651c42ee1b444179aa
Anastasia-bs <anniebondarenko@gmail.com>
2019-12-09 22:17:33 +0300

    check

--- lab_3/main.py
+++ lab_3/main.py
@@ -12,33 +12,107 @@ if __name__ == '__main__':
 
 
 class WordStorage:
+    def __init__(self):
+        self.storage = {}
+
     def put(self, word: str) -> int:
-        pass
+        if isinstance(word, str) and word not in self.storage:
+            self.storage[word] = len(self.storage) + 1
+            return self.storage[word]
 
     def get_id_of(self, word: str) -> int:
-        pass
+        if isinstance(word, str):
+            if word in self.storage:
+                return self.storage[word]
+        return -1
 
     def get_original_by(self, id: int) -> str:
-        pass
+        if id in self.storage.values():
+            for k, v in self.storage.items():
+                if id == v:
+                    return k
+        return 'UNK'
 
     def from_corpus(self, corpus: tuple):
-        pass
+        if isinstance(corpus, tuple):
+            for word in corpus:
+                self.put(word)
+        return self.storage
 
 
 class NGramTrie:
+    def __init__(self, n):
+        self.size = n
+        self.gram_frequencies = {}
+        self.gram_log_probabilities = {}
+
     def fill_from_sentence(self, sentence: tuple) -> str:
-        pass
+        gram_list = []
+        if isinstance(sentence, tuple):
+            for i in range(len(sentence)):
+                if len(sentence[i:i+self.size]) == self.size:
+                    gram_list.append(sentence[i:i+self.size])
+            for gram in gram_list:
+                if gram not in self.gram_frequencies:
+                    self.gram_frequencies[gram] = 1
+                else:
+                    self.gram_frequencies[gram] += 1
+        if self.gram_frequencies:
+            return 'OK'
+        else:
+            return 'ERROR'
 
     def calculate_log_probabilities(self):
-        pass
+        for gram in self.gram_frequencies:
+            all_grams = 0
+            for other_gram in self.gram_frequencies:
+                if gram[:-1] == other_gram[:-1]:
+                    all_grams += self.gram_frequencies[other_gram]
+            self.gram_log_probabilities[gram] = math.log(self.gram_frequencies[gram] / all_grams)
+        return self.gram_log_probabilities
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        pass
+        if isinstance(prefix, tuple) and len(prefix) == self.size-1:
+            prefix = list(prefix)
+            most_prob = [self.gram_log_probabilities[gram] for gram in self.gram_log_probabilities if
+                         list(gram)[:-1] == prefix]
+            if most_prob:
+                most_prob = max(most_prob)
+                for k, v in self.gram_log_probabilities.items():
+                    if most_prob == v and list(k)[:-1] == prefix:
+                        prefix.append(k[-1])
+            return prefix
+        return []
 
 
 def encode(storage_instance, corpus) -> list:
-    pass
+    if isinstance(storage_instance, WordStorage) and isinstance(corpus, list):
+        for sentence in corpus:
+            storage_instance.from_corpus(tuple(sentence))
+        storage_instance.from_corpus(tuple(corpus))
+        for sentence in corpus:
+            for i, token in enumerate(sentence):
+                sentence[i] = storage_instance.get_id_of(token)
+        return corpus
 
 
 def split_by_sentence(text: str) -> list:
-    pass
+    sentences = []
+    if text and ('.' in text or '?' in text or '!' in text):
+        for symbol in text:
+            if not symbol.isalpha() and symbol not in '.?! \n':
+                text = text.replace(symbol, '')
+        text = text.split()
+        text = ' '.join(text)
+        if text[-1] in '.!?':
+            text = text.replace(text[-1], '')
+        for i in range(len(text) - 2):
+            if text[i:i + 2] in ('? ', '! ', '. ') and text[i + 2].isupper():
+                text = text.replace(text[i:i + 2], '//')
+        if text:
+            sentences = text.lower().split('//')
+            for i, sentence in enumerate(sentences):
+                sentences[i] = sentence.split()
+                sentences[i].insert(0, '<s>')
+                sentences[i].insert(len(sentence), '</s>')
+    return sentences
